24ab7f67810b15a68148aa8b18da7e11429d98e0
gerikov <developer@gerikov.hu>
2025-06-14 16:44:16 +0200

    style loader

--- src/components/common/Loading.tsx
+++ src/components/common/Loading.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Loading: React.FC = () => {
   return (
-    <div className='fixed inset-0 bg-white flex items-center justify-center z-50'>
+    <div className='fixed inset-0 bg-background flex items-center justify-center z-50'>
       <div className='flex flex-col items-center'>
-        <div className='animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-gray-900'></div>
-        <p className='mt-4 text-gray-600'>Loading...</p>
+        <div className='animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-primary'></div>
+        <p className='mt-4 text-text-alter'>Loading...</p>
       </div>
     </div>
   )
